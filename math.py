115b6124df7f358e3e1f1f093b565db5fe604ac3
Srabonti Sen Neeti <105552240+SrabontiSenNeeti@users.noreply.github.com>
2022-05-17 13:51:12 +0600

    Create math.py

---
+++ math.py
@@ -0,0 +1,72 @@
+#math function
+import math
+a= 50.6
+print(math.ceil(a))
+print(math.floor(a))
+print(math.e)
+print(math.pi)
+print(math.tau)
+
+#Area
+r=3 #radius
+pie = math.pi
+print(pie*r*r) #area
+
+#Factorial
+a=5
+print(math.factorial(a))
+
+
+#Finding the absolute Value
+a= -20
+print(math.fabs(a))
+print(abs(a))
+
+
+#Finding the power of exp
+a = 4
+b = -3
+c = 0.0
+print(math.exp(a))
+print(math.exp(b))
+print(math.exp(c))
+
+#finding the power of a number
+a = 3
+b = 4
+print(pow(a,b))
+print(pow(3,3))
+
+#Finding the Logarithm
+a = 2
+b = 3
+print(math.log(a,b))
+print(math.log2(16))
+print(math.log10(10000))
+
+
+#Finding the square root
+print(math.sqrt(0))
+print(math.sqrt(4))
+print(math.sqrt(9.5))
+
+
+#Trigonomatric and Angular functions
+a=math.pi/6
+print(math.sin(a))
+print(math.cos(a))
+print(math.tan(a))
+
+print(math.sin(30))
+print(math.cos(30))
+print(math.tan(30))
+
+#Converting values from degree to radians and vice versa
+a = math.pi/6
+b = 30
+print(math.degrees(a))
+print(math.radians(b))
+
+#Finding gamma value
+a = 6
+print(math.gamma(a))
